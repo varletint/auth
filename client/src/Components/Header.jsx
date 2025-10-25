cfb41df8f14d69fa351c6835a7201edae41677a4
varletint <intvarlet@gmail.com>
2025-10-25 14:02:01 +0100

    .env

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,19 +7,19 @@ export default function Header() {
     <div
       className=' header w-[1500px] max-w-[95%]  m-auto
       flex justify-between items-center relative z-[100] px-[10px] py-[15px]
-       bg-green-500 '>
+        '>
       <Link
         to='/'
         className=' font-bold text-[25px] text-nowrap
       '>
         Lookups
       </Link>
-      <ul className=' hidden sm:flex  gap-[10px] justify-end font-medium'>
+      <ul className=' hidden sm:flex  gap-[10px] justify-end font-bold'>
         <li>
-          <Link to={"/register"}>sign up</Link>
+          <Link to={"/register"}>Account</Link>
         </li>
-        <li>sign up</li>
-        <li>sign up</li>
+        {/* <li>sign up</li>
+        <li>sign up</li> */}
       </ul>
 
       <button className='flex sm:hidden justify-end font-semibold '>
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,15 +1,51 @@
 import React from "react";
 import Header from "../Components/Header";
 import Input from "../Components/Input";
+import {
+  AddCircleHalfDotIcon,
+  Doc01Icon,
+  Doc02Icon,
+  ListViewIcon,
+  ProductLoadingIcon,
+  Profile02Icon,
+  ProfileIcon,
+  PropertyAddIcon,
+  UserIcon,
+  ViewIcon,
+} from "hugeicons-react";
 
 export default function Profile() {
   return (
     <>
       {/* <Header /> */}
       <div className='flex flex-row '>
-        <div className=' min-h-screen lg:w-52 sm:w-40 bg-gray-500'></div>
-        <div className=' bg-red-600 min-h-[1500px] w-full '>
-          <div className=' min-h-[8vh] bg-amber-300'></div>
+        <div
+          className='  h-[100vh] lg:w-60 sm:w-52 hidden sm:block bg-gray-500 sticky
+        overflow-y-none top-0  '>
+          <div className='px-2 flex flex-col gap-5'>
+            <div className=' py-2.5 h-[50px] '>
+              <h1 className=' text-lg font-semibold'>Dashboard</h1>
+            </div>
+            <div className=' font-semibold '>
+              <ul className=' text-sm flex flex-col gap-4'>
+                <li className='flex items-center gap-1'>
+                  <UserIcon size={18} /> Profile
+                </li>
+                <li className='flex items-center gap-1 text-nowrap'>
+                  <Doc02Icon size={18} /> Overview
+                </li>
+                <li className='flex items-center gap-1 text-nowrap'>
+                  <ProductLoadingIcon size={18} /> My Products
+                </li>
+                <li className='flex items-center gap-1 text-nowrap'>
+                  <AddCircleHalfDotIcon size={18} /> Add product
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className=' bg-red-600 min-h-[1500px w-full '>
+          <div className=' min-h-[50px] bg-amber-300'></div>
           <div className='w-full bg-red-200 p-3'>
             <div className='h-[] bg-yellow-700 p-1.5'>
               <div className='flex flex-col sm:flex-row  items-center gap-2.5'>
@@ -48,6 +84,11 @@ export default function Profile() {
                 {/* <div className='bg-red-500 h-20'>1</div>
                 <div className='bg-red-500 h-20'>1</div>
                 <div className='bg-red-500 h-20'>1</div> */}
+                <div className='card'></div>
+                <div className='card'></div>
+                <div className='card'></div>
+                <div className='card'></div>
+                <div className='card'></div>
                 <div className='card'></div>
                 <div className='card'></div>
                 <div className='card'></div>
