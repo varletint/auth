1a1032ee93d3205827a47691ec4feff5af77a1f4
varletint <intvarlet@gmail.com>
2025-10-27 00:07:11 +0100

    ll

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-import e from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from "express";
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import cors from "cors";
 
-const app = e();
-app.use(cors());
-app.use(e.json());
-dotenv.config();
-// app.listen(3000, () => console.log("server connected"));
+// const app = e();
+// app.use(cors());
+// app.use(e.json());
+// dotenv.config();
+// // app.listen(3000, () => console.log("server connected"));
 
-app.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "hell",
-  });
+// app.get("/", async (req, res) => {
+//   res.status(200).json({
+//     message: "hell",
+//   });
+// });
+// // mongoose
+// //   .connect(process.env.MONGO_KEYS)
+// //   .then(() => console.log("db connected"))
+// //   .catch((err) => console.log(err));
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal server error";
+
+//   res.status(statusCode).json({
+//     success: false,
+//     message,
+//     statusCode,
+//   });
+// });
+
+// module.exports = app;
+
+// const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel backend!");
 });
-// mongoose
-//   .connect(process.env.MONGO_KEYS)
-//   .then(() => console.log("db connected"))
-//   .catch((err) => console.log(err));
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
-
-  res.status(statusCode).json({
-    success: false,
-    message,
-    statusCode,
-  });
-});
-
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
