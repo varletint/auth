36166f63ad7d98167d8a708007bbe127db357d64
varletint <intvarlet@gmail.com>
2025-11-01 00:33:09 +0100

    res

--- index.js
+++ index.js
@@ -55,4 +55,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+app.get("/", (req, res) => {
+  res.send("wwwwww");
+});
 app.use("/api", testingRoute);
