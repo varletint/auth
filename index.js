70f62fe96eacf67eecc4c6c56567eaa8d6107b63
varletint <intvarlet@gmail.com>
2025-11-01 00:10:37 +0100

    ad

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ import testingRoute from "./Routes/whatsapp.webhook.route.js";
 
 const app = express();
 
-app.get("/api", testingRoute);
+app.use("/api", testingRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
