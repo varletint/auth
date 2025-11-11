6588f948250e355263f2706ca2d12455bf986ecf
varletint <intvarlet@gmail.com>
2025-11-11 04:11:22 +0100

    feat: add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use(cors());
 app.get("/testing", (req, res) => {
   res.send("Testing endpoint is working!");
 });
