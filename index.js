e83488af4f73e7622433caaf437b4c49ce996c93
varletint <intvarlet@gmail.com>
2025-11-11 04:44:32 +0100

    fix: simplify CORS configuration by using default settings

--- index.js
+++ index.js
@@ -18,17 +18,7 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use(
-  cors({
-    origin: [
-      "https://lookupsclient.vercel.app",
-      "http://localhost:5173",
-      "https://auth-fawn-eight.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.get("/testing", (req, res) => {
   res.send("Testing endpoint is working!");
 });
@@ -36,6 +26,5 @@ app.get("/testing", (req, res) => {
 app.post("/webhook", (req, res) => {
   const { name } = req.body;
   console.log("Received name:", name);
-  alert("Webhook received with name: " + name);
   res.json({ message: "Webhook received", name });
 });
