3e614a58cf2f8c3b962d53550d6ce12a2708086d
varletint <intvarlet@gmail.com>
2025-11-11 03:47:15 +0100

    feat: add MongoDB connection setup to the application

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_KEYS)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
