b3b9875d72430cd01ed543540d9cc431b1eee693
varletint <intvarlet@gmail.com>
2025-11-17 21:42:16 +0100

    hi

--- controller/respose.js
+++ controller/respose.js
@@ -43,18 +43,18 @@ export const responseMessage = async (req, res) => {
     const user = message.from;
     const text = message.text?.body?.toLowerCase();
 
-    if (text === "hi") {
+    if (text.toLowerCase().trim() === "hi") {
       const waRes = await fetch(
         "https://graph.facebook.com/v22.0/886326117894676/messages",
         {
           method: "POST",
           headers: {
-            Authorization: `Bearer EAALamZBywGWUBPxt5cnGOZBGRJkLvLUcO4ZC2BpAMZCJngqIphCD01i9u14ONSlKwDhsbZAWguqGE2vJr6mUOQCR8bkzP19ZAZCJJ4ayX4YNDmD23b1n68eAwYRsllyqWibcqKBRQcZBJwl0mc3avncVnXOPhs7zSNrICbpYLpswAzvsFJ5g6Xm23rs5LjYwMRhv9BRFiWqH8o4YpRoMDMtBkJjm9WzsPhFwWUWZA9EIglmmBCYUZD`,
+            Authorization: `Bearer EAALamZBywGWUBPzHoziI9yZAJdkpyfLZBvnNtFJA6fipWDpq9tCpK2iAEnC4BGyZAZAiCckSnakPiWyK6Gi0ZBzrRe2m6GYZCvUsgVFXS7ZAEyzjUAMyqFQ64Qz5xcXHAPZCIZAFHnJHDcAmfJtddD0U0ZB1XNNGZBBM97lzpCx6Ux0DawlGn2ZA9bXelUEawyNXdjgxHYoekav7P2L81SkSFZBZA3Dx8P73wcmqMGwNGdc2SFx8MP9cAZDZD`,
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
             messaging_product: "whatsapp",
-            to: user,
+            to: "2347063255405",
             type: "text",
             text: {
               body: "Hello! How can I assist you today?",
@@ -67,7 +67,7 @@ export const responseMessage = async (req, res) => {
       console.log("WA Response:", waData);
     }
 
-    return res.sendStatus(200); // IMPORTANT
+    return res.sendStatus(200);
   } catch (error) {
     console.error("Webhook error:", error);
     return res.sendStatus(500);
