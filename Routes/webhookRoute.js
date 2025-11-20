589120af8fce5894d0ff3e37afc81ced8818dfa6
varletint <intvarlet@gmail.com>
2025-11-20 11:19:44 +0100

    c

--- Routes/webhookRoute.js
+++ Routes/webhookRoute.js
@@ -1,9 +1,37 @@
 import express from "express";
-import { responseMessage } from "../controller/webhookController.js";
+import { sendText } from "../services/whatsapp.js";
+// import { responseMessage } from "../controller/webhookController.js";
 
 const router = express.Router();
 
-router.post("/webhook", responseMessage);
+router.post("/webhook", async () => {
+  try {
+    const entry = req.body.entry?.[0];
+    const changes = entry?.changes?.[0];
+    const message = changes?.value?.messages?.[0];
+
+    if (!message) {
+      return res.status(400).json({ error: "No message found in webhook" });
+    }
+
+    const from = message.from;
+    const text = message.text?.body;
+
+    if (text.toLowerCase().trim() === "hi".trim()) {
+      await sendText(from, "working");
+      return res.sendStatus(200);
+    }
+    res.sendStatus(200);
+  } catch (err) {
+    const entry = req.body.entry?.[0];
+    const msg = entry?.changes?.[0]?.value?.messages?.[0];
+    const from = msg.from;
+
+    await sendText(from, `error: ${err}`);
+    console.error("WebhookController error", err);
+    res.sendStatus(500);
+  }
+});
 
 export default router;
 
