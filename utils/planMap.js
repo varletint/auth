e5d46802bb52c902bf250877abfba31930aaf790
varletint <intvarlet@gmail.com>
2025-11-22 23:03:28 +0100

    plan

--- utils/planMap.js
+++ utils/planMap.js
@@ -1,51 +1,51 @@
-// export const PLAN_MAP = {
-//   plan_500: {
-//     amount: 500,
-//     desc: "₦500 - 1GB",
-//   },
-//   plan_1000: {
-//     amount: 1000,
-//     desc: "₦1000 - 2.5GB",
-//   },
-//   plan_1500: {
-//     amount: 1500,
-//     desc: "₦1500 - 4GB",
-//   },
-// };
-
-export const plan_map = {
-  MTN: {
-    plan_500: {
-      amount: 500,
-      desc: "₦500 - 1GB",
-      network: "mtn",
-    },
-    plan_1000: {
-      amount: 1000,
-      desc: "₦1000 - 2.6GB",
-      network: "mtn",
-    },
-    plan_1500: {
-      amount: 1500,
-      desc: "₦1500 - 4GB",
-      network: "mtn",
-    },
+export const PLAN_MAP = {
+  plan_500: {
+    amount: 500,
+    desc: "₦500 - 1GB",
   },
-  AIRTEL: {
-    plan_500: {
-      amount: 500,
-      desc: "₦500 - 1GB",
-      network: "airtel",
-    },
-    plan_1000: {
-      amount: 1000,
-      desc: "₦1000 - 2.5GB",
-      network: "airtel",
-    },
-    plan_1500: {
-      amount: 1500,
-      desc: "₦1500 - 4GB",
-      network: "airtel",
-    },
+  plan_1000: {
+    amount: 1000,
+    desc: "₦1000 - 2.5GB",
+  },
+  plan_1500: {
+    amount: 1500,
+    desc: "₦1500 - 4GB",
   },
 };
+
+// export const plan_map = {
+//   MTN: {
+//     plan_500: {
+//       amount: 500,
+//       desc: "₦500 - 1GB",
+//       network: "mtn",
+//     },
+//     plan_1000: {
+//       amount: 1000,
+//       desc: "₦1000 - 2.6GB",
+//       network: "mtn",
+//     },
+//     plan_1500: {
+//       amount: 1500,
+//       desc: "₦1500 - 4GB",
+//       network: "mtn",
+//     },
+//   },
+//   AIRTEL: {
+//     plan_500: {
+//       amount: 500,
+//       desc: "₦500 - 1GB",
+//       network: "airtel",
+//     },
+//     plan_1000: {
+//       amount: 1000,
+//       desc: "₦1000 - 2.5GB",
+//       network: "airtel",
+//     },
+//     plan_1500: {
+//       amount: 1500,
+//       desc: "₦1500 - 4GB",
+//       network: "airtel",
+//     },
+//   },
+// };
