710532c42437f0a2c4b5bb87e1d112272507976e
varletint <intvarlet@gmail.com>
2025-12-01 22:06:56 +0100

    fix: Set cookie sameSite to none for cross-origin support

--- controller/authController.js
+++ controller/authController.js
@@ -52,13 +52,13 @@ export const signup = async (req, res, next) => {
       .cookie("access_token", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000 // 15 minutes
       })
       .cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
       })
       .status(201)
@@ -137,13 +137,13 @@ export const signin = async (req, res, next) => {
       .cookie("access_token", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000 // 15 minutes
       })
       .cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
       })
       .status(200)
