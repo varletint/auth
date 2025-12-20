137d3de9ee3170f5268430d4d783b84d23d1f1a2
varletint <intvarlet@gmail.com>
2025-12-20 23:52:54 +0100

    pure devil

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ const allowedOrigins = [
   "https://lookupss.vercel.app",
   "http://localhost:5173",
   "http://192.168.137.1:5173",
-  "http://10.160.189.108:5173"
+  "http://10.160.189.108:5173",
+  "http://10.22.6.108:5173",
+  "http://192.168.137.1:5173"
 ];
 app.use(
   cors({
